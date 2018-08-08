96f81611acc21d932fb3ef5c571ffad077bbecf4
Sai Maansa N <40040859+Manasa-Sai@users.noreply.github.com>
2018-08-08 15:11:12 -0400

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -5,18 +5,10 @@ import sklearn
 
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction import text
-from sklearn.svm import LinearSVC
+
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.multiclass import OneVsOneClassifier
-from nltk import word_tokenize          
-from nltk.stem.porter import PorterStemmer
-from sklearn import svm
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
+
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
@@ -35,18 +27,19 @@ traindata = rawtrain.fillna(0)
 testdata = rawtest.fillna(0)
 
 
-text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('svc', OneVsOneClassifier(SVC(kernel='linear')))])
+
 phrases = []
 for x in range(0,len(traindata['Phrase'])):
     phrases.append(traindata['Phrase'][x])
 
-text_clf = text_clf.fit(phrases, traindata['Sentiment'])
+classifier = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('dt', DecisionTreeClassifier())])
+classifier = classifier.fit(phrases, traindata['Sentiment'])
 
 phraseTest = []
 for x in range(0,len(testdata['Phrase'])):
     phraseTest.append(testdata['Phrase'][x])
 
-predictedData = text_clf.predict(phraseTest)
+predictedData = classifier.predict(phraseTest)
 
 with open(r"Desktop\all\sampleSubmission.csv", "w") as subm:
     subm.write("PhraseID, Sentiment\n")
